bee5e13f275b0da2118e77d3204c0eabe082459d
Aman Ullah <amanix007@gmail.com>
2022-02-17 20:08:33 +0600

    works

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './style.css'
+const getFullName = (name: string, surname: string): string => {
+  return name + " " + surname;
+};
 
-const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+console.log(getFullName("Aman", "Ullah"));
